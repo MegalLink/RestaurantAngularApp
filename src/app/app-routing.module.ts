e70c5fb0c31d227ff2589172690047efba6bad5e
MegalLink <alexresidentevil@gmail.com>
2021-04-27 16:48:37 -0500

    Inicio page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {InicioComponent} from './pages/inicio/inicio.component'
+import {ContactoComponent} from './pages/contacto/contacto.component'
+import {EventosComponent} from './pages/eventos/eventos.component'
+import { MenuComponent } from './pages/menu/menu.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+const routes: Routes = [
+  {path:'', component:InicioComponent},
+  {path:'menu', component:MenuComponent},
+  {path:'contacto', component:ContactoComponent},
+  {path:'eventos', component:EventosComponent},
+  {path:'nosotros', component:NosotrosComponent},
+  {path:'ofertas', component:OfertasComponent},
+  {path:'**',pathMatch:'full',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+import { EventosComponent } from './pages/eventos/eventos.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    NosotrosComponent,
+    MenuComponent,
+    OfertasComponent,
+    EventosComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
